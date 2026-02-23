0169fcf33a72dd4a758bbdbe7e10d30768f0e6a2
Davit Tchanturia <davit.tchanturia.1@btu.edu.ge>
2026-02-23 12:23:12 +0400

    feature: deploys cheduler for bronze job

---
+++ terraform/scheduler.tf
@@ -0,0 +1,31 @@
+resource "google_cloud_scheduler_job" "nyc_taxi_bronze_ingestion" {
+  name        = "nyc-taxi-bronze-monthly-ingestion"
+  description = "Triggers NYC Taxi bronze ingestion CF on the 1st of every month"
+  schedule    = "0 0 1 * *"
+  time_zone   = "UTC"
+  project     = var.project_id
+  region      = var.region
+
+  http_target {
+    uri         = "https://trigger-bronze-ingestion-344181310958.europe-west1.run.app"
+    http_method = "POST"
+
+    headers = {
+      "Content-Type" = "application/json"
+    }
+
+    body = base64encode(jsonencode({
+      write_mode = "overwrite"
+    }))
+
+    oidc_token {
+      service_account_email = google_service_account.scheduler_sa.email
+      audience              = "https://trigger-bronze-ingestion-344181310958.europe-west1.run.app"
+    }
+  }
+
+  depends_on = [
+    google_project_iam_member.scheduler_cf_invoker,
+    google_project_iam_member.scheduler_run_invoker,
+  ]
+}
